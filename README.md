c1ab018f6cc6832619b80e561e5364af5dbe957e
Abhinav Gupta <abhinavgupta203@gmail.com>
2022-07-19 17:05:46 -0400

    adding preprocessed data and scripts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ehr-benchmarking
+
+Used outlier thresholds from [Harutyunyan et al.](https://github.com/YerevaNN/mimic3-benchmarks).
---
+++ scripts/dataset_path.py
@@ -0,0 +1,4 @@
+## Defining "argparse" parameters
+
+mimic_iv_path = '/Users/abhinav/Desktop/IS-EHR/mimic/mimic-iv-1.0'
+output_path = '/Users/abhinav/Desktop/IS-EHR/mimic/mimic-iv-1.0/output/filtered'

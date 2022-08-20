92138d032dafb52fcde2f3235a2bf9b442794888
ag2307 <abhinavgupta203@gmail.com>
2022-08-20 06:52:45 -0400

    added hyperparameter tuning and updated preprocessing scripts

--- scripts/dataset_path.py
+++ scripts/dataset_path.py
@@ -1,4 +1,11 @@
 ## Defining "argparse" parameters
 
-mimic_iv_path = '/Users/abhinav/Desktop/IS-EHR/mimic/mimic-iv-1.0'
-output_path = '/Users/abhinav/Desktop/IS-EHR/mimic/mimic-iv-1.0/output/filtered'
+#For ZJ
+mimic_iv_path = '/gpfs/scratch/ag7948/mimic/mimic-iv-1.0'
+output_path = '/gpfs/scratch/ag7948/mimic/mimic-iv-1.0/output'
+
+#mimic_iv_path = '/Users/jin/Desktop/Temporal_AutoScore/Data/mimic-iv-1.0'
+#mimic_iv_path = '/Users/siqili/Desktop/National University of Singapore/Liu Nan - WP_MIMIC_Benchmark/OriginalData/data'
+
+#output_path = '/Users/jin/Desktop/Temporal_AutoScore/Output/mimic-iv-1.0'
+#output_path = '/Users/siqili/Desktop/National University of Singapore/Liu Nan - WP_MIMIC_Benchmark'
